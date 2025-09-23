7a8519e2b9212447fd34eacbaca366c1d1f09a40
Vaishnavi Kute <kutevaishnavi9@gmail.com>
2025-09-23 17:11:55 +0530

    Time: 0 ms (100%), Space: 18 MB (17.28%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1 = list(map(int, version1.split(".")))
+        v2 = list(map(int, version2.split(".")))
+        
+        # Pad the shorter list with zeros
+        n = max(len(v1), len(v2))
+        v1.extend([0] * (n - len(v1)))
+        v2.extend([0] * (n - len(v2)))
+        
+        # Compare each revision
+        for a, b in zip(v1, v2):
+            if a < b:
+                return -1
+            elif a > b:
+                return 1
+        
+        return 0
+
+        
