fbcce05f373882185a5d4c009df5224baeafb321
Vaishnavi Kute <kutevaishnavi9@gmail.com>
2025-09-25 23:33:20 +0530

    Time: 3 ms (86%), Space: 19.1 MB (46.7%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # Start from the second last row and move upwards
+        for i in range(len(triangle) - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+        
+        # The top element now contains the minimum path sum
+        return triangle[0][0]
+
+
+        
