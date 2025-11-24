263fc4bc26df2e6d73d8ff99c6e126243b5a6f6f
Vaishnavi Kute <kutevaishnavi9@gmail.com>
2025-11-24 17:06:32 +0530

    Time: 3 ms (62.61%), Space: 17.6 MB (96.04%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,20 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        left, right = 1, x // 2
+        
+        while left <= right:
+            mid = (left + right) // 2
+            sq = mid * mid
+            
+            if sq == x:
+                return mid
+            elif sq < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return right  # right is the floor(sqrt(x))
+        
