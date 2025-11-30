0bf3409e6e2d691bcfdbc4b54f08ca8c199132b4
Vaishnavi Kute <kutevaishnavi9@gmail.com>
2025-11-30 11:42:34 -0500

    Time: 84 ms (27.47%), Space: 38.1 MB (59.41%) - LeetHub

---
+++ 1590-make-sum-divisible-by-p/1590-make-sum-divisible-by-p.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        total = sum(nums)
+        need = total % p
+        if need == 0:
+            return 0
+
+        prefix = 0
+        seen = {0: -1} # prefix sum % p : index
+        res = len(nums)
+
+        for i, num in enumerate(nums):
+            prefix = (prefix + num) % p
+
+            #We want prefix[i] == (prefix - need +p) % p
+            target = (prefix - need) % p
+
+            if target in seen:
+                res = min(res, i - seen[target])
+            seen[prefix] = i
+        return res if res != len(nums) else -1
+        
