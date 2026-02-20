30e4948a298a2e95bed0739d6469640dab9a62c4
Vaishnavi Kute <kutevaishnavi9@gmail.com>
2026-02-20 22:10:38 +0530

    Time: 0 ms (100%), Space: 19.3 MB (65.79%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        parts = []
+        count = 0
+        start = 0
+
+        for i, ch in enumerate(s):
+            if ch == '1':
+                count += 1
+            else:
+                count -= 1
+
+            # when a special substring ends
+            if count == 0:
+                inner = s[start + 1:i]
+                parts.append("1" + self.makeLargestSpecial(inner) + "0")
+                start = i + 1
+
+        # sort substrings in descending lexicographic order
+        parts.sort(reverse=True)
+
+        return "".join(parts)        
